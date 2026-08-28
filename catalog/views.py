381f9009b639321570a60bc2e700ec76b417184e
Keaton <fostygera@gmail.com>
2026-08-28 14:27:36 +0300

    add product detail and template inheritance

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,18 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from .models import Product
+
 
 def home(request: HttpRequest) -> HttpResponse:
-    return render(request, "catalog/home.html")
+    products = Product.objects.all()
+    return render(request, "catalog/home.html", {"products": products})
 
 
 def contacts(request: HttpRequest) -> HttpResponse:
     return render(request, "catalog/contacts.html")
+
+
+def product_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    product = Product.objects.get(pk=pk)
+    return render(request, "catalog/product_detail.html", {"product": product})
